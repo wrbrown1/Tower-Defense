b5c653cd2b21a78e3e30f47a36a1dc5640fbad14
Will <wrbrown@aggies.ncat.edu>
2019-11-03 18:47:13 -0500

    Added grid system

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+public class CubeEditor : MonoBehaviour
 {
+
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    void Start()
+    {
+
+    }
+
     void Update()
     {
         Vector3 snap;
         snap.x = Mathf.RoundToInt(transform.position.x / 10f) * gridSize;
         snap.z = Mathf.RoundToInt(transform.position.z / 10f) * gridSize;
-
         transform.position = new Vector3(snap.x, 0f, snap.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        string spot = snap.x / gridSize + ", " + snap.z / gridSize;
+        textMesh.text = spot;
+        gameObject.name = spot;
     }
 }
---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] List<Block> path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Block block in path)
+        {
+            print(block.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
