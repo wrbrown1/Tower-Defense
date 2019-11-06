909c2d476fefa9ecd88e24a854d5b3e7f5bf5174
Will <wrbrown@aggies.ncat.edu>
2019-11-05 19:22:30 -0500

    Added tower funtionality

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+
+    [SerializeField] Collider collisionMesh;
+    [SerializeField] int health;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print("Collision detected");
+        health = health - 10;
+        if(health <= 0)
+        {
+            Destroy(gameObject, 1f);
+        }
+    }
+
+}
---
+++ Assets/Tower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectPan;
+    [SerializeField] Transform targetEnemy;
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        objectPan.LookAt(targetEnemy);
+    }
+}
